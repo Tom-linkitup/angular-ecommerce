aa06124e31ec8d56d8a1292de57469243ec39435
tomgu <atom8708@gmail.com>
2023-02-01 13:56:44 +0000

    edit production url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  lu2shopApiUrl: "https://ecommerce-spring-boot.herokuapp.com/api",
+  stripePublishableKey: "pk_test_51ML5KaJJ5YSREscgDkaBEKwhx3frlZ0zKDSQyDvXw8T8xub8Pffh3ngFnN3zvZnBLRR01ibXOnpDGEq3Rj4HxJqm00sSGl4g2p"
 };
